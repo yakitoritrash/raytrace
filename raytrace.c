fb1aef14d68a50e826d6c29e4da16758552cf0fb
Arch me <kishudwivedi30@gmail.com>
2025-05-19 11:12:08 +0530

    34234

--- raytrace.c
+++ raytrace.c
@@ -2,12 +2,13 @@
 #include <SDL2/SDL.h>
 
 #define WIDTH 900
-#define HEIGHT 900
+#define HEIGHT 600
 
 
 int main() {
   SDL_Init(SDL_INIT_VIDEO);
   SDL_Window* window = SDL_CreateWindow("Raytracing", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0);
 
+  SDL_Surface* surface = SDL_GetWindowSurface(window);
   SDL_Delay(5000);
 }
